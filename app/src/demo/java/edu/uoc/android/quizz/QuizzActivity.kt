2571fa370f8234136dd626baa1747738987abec0
Andrés Signes <asignes86@gmail.com>
2020-05-31 12:20:37 +0200

    flavors demo i full

---
+++ app/src/demo/java/edu/uoc/android/quizz/QuizzActivity.kt
@@ -0,0 +1,45 @@
+package edu.uoc.android.quizz
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.firestore.FirebaseFirestore
+import edu.uoc.android.R
+import kotlinx.android.synthetic.main.activity_quizz.*
+
+class QuizzActivity : AppCompatActivity() {
+    private val TAG = QuizzActivity::class.simpleName
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_quizz)
+
+        //RV manager
+        rv_quizz.layoutManager = LinearLayoutManager(this)
+
+        getQuestions()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        FirebaseAnalytics.getInstance(applicationContext)
+            .setCurrentScreen(this, this::class.java.simpleName, null)
+    }
+
+    private fun getQuestions() {
+        val questions: MutableList<Question> = mutableListOf()
+        FirebaseFirestore.getInstance().collection("Quizzes")
+            .limit(1)
+            .get()
+            .addOnSuccessListener { documents ->
+                for (document in documents) {
+                    questions.add(Question(document))
+                }
+                Log.d(TAG, "Numero de preguntas: ${questions.size}")
+                rv_quizz.adapter =
+                    MyquestionItemRecyclerViewAdapter(questions, this)
+            }
+    }
+}
